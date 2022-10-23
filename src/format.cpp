5ec6a88050282ec39a0125cb64ceb44142641ae3
Karina Romero <betzy_skamm@hotmail.com>
2022-10-23 13:10:15 -0500

    remove warnings

--- src/format.cpp
+++ src/format.cpp
@@ -11,7 +11,7 @@ using namespace std::literals::string_literals;
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
 string Format::ElapsedTime(long int totalSeconds) { 
-	tm curr_tm;
+	tm curr_tm = {};
 	char output_string[15];
     int hour, minutes, seconds;
 
@@ -21,7 +21,9 @@ string Format::ElapsedTime(long int totalSeconds) {
     totalSeconds %= 60;
     seconds = totalSeconds;
 
-	curr_tm = {.tm_sec=seconds, .tm_min=minutes, .tm_hour=hour};
+    curr_tm.tm_sec = seconds;
+    curr_tm.tm_min = minutes;
+    curr_tm.tm_hour = hour;
 	strftime(output_string, 15, "%T", &curr_tm);
     
     return output_string;
